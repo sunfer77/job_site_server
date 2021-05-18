da3190d848c0846e559cdfaae33a1e84b30c3ab2
sunfer77 <67311985+sunfer77@users.noreply.github.com>
2021-05-18 16:42:12 +0200

    -

--- Index.js
+++ Index.js
@@ -42,7 +42,7 @@ app.use(
 		secret: 'secret',
 		resave: false,
 		saveUninitialized: false,
-		cookie: { secure: true, maxAge: 24 * 60 * 60 * 1000 },
+		cookie: { secure: true, httpOnly: true, maxAge: 24 * 60 * 60 * 1000 },
 	})
 );
 app.get('/', (req, res) => {
