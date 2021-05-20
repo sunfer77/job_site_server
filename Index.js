2bd35ba86c92b3deae29b3bcc772a9ba6b9e7a44
sunfer77 <67311985+sunfer77@users.noreply.github.com>
2021-05-20 13:46:31 +0200

    moved build file from client folder to server to resolve cookie issue and upload to heroku

--- Index.js
+++ Index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 app.set('trust proxy', 1);
 app.use(
 	cors({
-		origin: 'http://localhost:3001',
+		//origin: 'https://job-app-react.herokuapp.com',
 		credentials: true,
 	})
 );
