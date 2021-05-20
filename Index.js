e1050a73a2545316966daadea85c0d57c0bb9df5
sunfer77 <67311985+sunfer77@users.noreply.github.com>
2021-05-20 14:40:46 +0200

    moved build file from client folder to server to resolve cookie issue and upload to heroku

--- Index.js
+++ Index.js
@@ -17,8 +17,10 @@ app.use(
 		secret: 'secret',
 		resave: false,
 		saveUninitialized: false,
+		unset: 'destroy',
 
 		cookie: {
+			sameSite: 'Lax',
 			httpOnly: true,
 			secure: true,
 			maxAge: 24 * 60 * 60 * 1000,
